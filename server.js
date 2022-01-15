ddf3a166261f9f41d6fa60f4974f84042fb67931
boa <boa.matule@gmail.com>
2022-01-15 12:17:09 +0100

    Updates the packages

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require('express')
 const cors = require('cors')
 const helmet = require('helmet')
 const winston = require('winston');
+// const morgan = require('morgan');
 
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const app = express()
 app.use(cors())
 app.use(helmet())
-app.use(bodyParser.json())
+app.use(express.urlencoded({extended: true})); 
+app.use(express.json());
+// app.use(morgan('combined'))
+// app.use(winston());
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
